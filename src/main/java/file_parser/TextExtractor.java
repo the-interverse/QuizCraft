fac9d262f17e6c497dce35ff2ad922b426c5fb94
egordolgov <egor.dolgov8@gmail.com>
2024-11-09 10:25:27 -0500

    Completed Controller,InputData,OutputData,Presenter. made TextExtractor to only work with pdfs. code runs without error during this commit

--- src/main/java/file_parser/TextExtractor.java
+++ src/main/java/file_parser/TextExtractor.java
@@ -5,8 +5,6 @@ public class TextExtractor {
     public static String extractText(String filePath) throws IOException {
         if (filePath.endsWith(".pdf")) {
             return PDFParser.parsePDF(filePath);
-        } else if (filePath.endsWith(".docx")) {
-            return WordParser.parseDocx(filePath);
         } else {
             throw new IllegalArgumentException("Unsupported file format");
         }
