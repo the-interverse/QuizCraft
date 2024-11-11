121feb5a49aa2d767b790319d115815a1a19f031
egordolgov <egor.dolgov8@gmail.com>
2024-11-11 09:47:43 -0500

    Some minor changes for createquiz

--- src/main/java/use_case/create_quiz/CreateQuizDataAccessInterface.java
+++ src/main/java/use_case/create_quiz/CreateQuizDataAccessInterface.java
@@ -1,6 +1,7 @@
 package use_case.create_quiz;
 
 
+import entity.Quiz;
 import entity.User;
 /**
  * DAO for the Login Use Case.
@@ -12,30 +13,11 @@ public interface CreateQuizDataAccessInterface {
      * @param username the username to look for
      * @return true if a user with the given username exists; false otherwise
      */
-    boolean existsByName(String username);
+    boolean existsQuizByUser(String username);
 
     /**
      * Saves the user.
-     * @param user the user to save
+     * @param quiz quiz to save
      */
-    void save(User user);
-
-    /**
-     * Returns the user with the given username.
-     * @param username the username to look up
-     * @return the user with the given username
-     */
-    User get(String username);
-
-    /**
-     * Returns the username of the curren user of the application.
-     * @return the username of the current user; null indicates that no one is logged into the application.
-     */
-    String getCurrentUsername();
-
-    /**
-     * Sets the username indicating who is the current user of the application.
-     * @param username the new current username; null to indicate that no one is currently logged into the application.
-     */
-    void setCurrentUsername(String username);
+    void saveQuiz(Quiz quiz);
 }
