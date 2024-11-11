d24fd313cd688d151d854030af1aa8b00adeacab
kirillutyashev <kirillut04@gmail.com>
2024-11-11 09:50:31 -0500

    Create tests for create quiz use case and minor updates for inmemory and file data access objects

---
+++ src/test/java/use_case/createquiz/CreateQuizInteractorTest.java
@@ -0,0 +1,27 @@
+package use_case.createquiz;
+
+import data_access.InMemoryUserDataAccessObject;
+import entity.User;
+import entity.UserFactory;
+import org.junit.jupiter.api.Test;
+import use_case.login.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class CreateQuizInteractorTest {
+    @Test
+    void successTest() {
+//        TODO
+    }
+
+    @Test
+    void successCreateQuizTest() {
+//        TODO
+    }
+
+    @Test
+    void failureCreateQuizTest() {
+//        TODO
+    }
+}
