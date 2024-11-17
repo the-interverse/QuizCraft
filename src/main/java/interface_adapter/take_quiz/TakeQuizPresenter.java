7fcdf2cf383f5aa71d3be5b28fd59b5c1a3832cb
kirillutyashev <kirillut04@gmail.com>
2024-11-17 16:42:04 -0500

    TakeQuiz use case implementation

--- src/main/java/interface_adapter/take_quiz/TakeQuizPresenter.java
+++ src/main/java/interface_adapter/take_quiz/TakeQuizPresenter.java
@@ -1,5 +1,33 @@
 package interface_adapter.take_quiz;
 
-public class TakeQuizPresenter {
-//    TODO
+import interface_adapter.ViewManagerModel;
+import use_case.take_quiz.TakeQuizOutputBoundary;
+import use_case.take_quiz.TakeQuizOutputData;
+import view.ViewManager;
+import view.ViewQuizView;
+
+public class TakeQuizPresenter implements TakeQuizOutputBoundary {
+    private final TakeQuizViewModel takeQuizViewModel;
+    private final ViewManagerModel viewManagerModel;
+    private final ViewQuizView viewQuizView;
+
+
+    public TakeQuizPresenter(TakeQuizViewModel takeQuizViewModel,
+                             ViewManagerModel viewManagerModel,
+                             ViewQuizView viewQuizView) {
+        this.takeQuizViewModel = takeQuizViewModel;
+        this.viewManagerModel = viewManagerModel;
+        this.viewQuizView = viewQuizView;
+    }
+
+    @Override
+    public void prepareSuccessView(TakeQuizOutputData response) {
+//  TODO
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+
+    }
+//  TODO
 }
